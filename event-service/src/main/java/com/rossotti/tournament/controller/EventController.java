a45cf9c3193077ba97f566ca2e1258fdadb67016
Paul Rossotti <rossotti.paul@gmail.com>
2020-03-07 21:07:49 -0800

    added createEvent() to EventController

---
+++ event-service/src/main/java/com/rossotti/tournament/controller/EventController.java
@@ -0,0 +1,54 @@
+package com.rossotti.tournament.controller;
+
+import com.rossotti.tournament.dto.EventDTO;
+import com.rossotti.tournament.exception.EntityExistsException;
+import com.rossotti.tournament.exception.NoSuchEntityException;
+import com.rossotti.tournament.jpa.enumeration.TemplateType;
+import com.rossotti.tournament.jpa.model.Event;
+import com.rossotti.tournament.jpa.model.Organization;
+import com.rossotti.tournament.jpa.service.EventJpaService;
+import com.rossotti.tournament.jpa.service.OrganizationJpaService;
+import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EventController {
+
+	private final EventJpaService eventJpaService;
+	private final OrganizationJpaService organizationJpaService;
+	private final Logger logger = LoggerFactory.getLogger(EventController.class);
+
+	@Autowired
+	public EventController(EventJpaService eventJpaService, OrganizationJpaService organizationJpaService) {
+		this.eventJpaService = eventJpaService;
+		this.organizationJpaService = organizationJpaService;
+	}
+
+	public Event createEvent(EventDTO eventDTO) {
+		Organization organization = organizationJpaService.findByOrganizationNameAsOfDate(eventDTO.getOrganizationName(), eventDTO.getEndDate());
+		if (organization != null) {
+			TemplateType templateType = TemplateType.valueOf(eventDTO.getTemplateType());
+			Event event = eventJpaService.findByOrganizationNameAsOfDateTemplateType(eventDTO.getOrganizationName(), eventDTO.getEndDate(), templateType);
+			if (event == null) {
+				ModelMapper modelMapper = new ModelMapper();
+				logger.debug("createEvent - saveEvent: orgName = " + eventDTO.getOrganizationName() + ", eventName = " + eventDTO.getEventName());
+				event = modelMapper.map(eventDTO, Event.class);
+				return eventJpaService.save(event);
+			}
+			else {
+				logger.debug("createEvent - findByOrganizationNameAsOfDateTemplateType: orgName = " + eventDTO.getOrganizationName() +
+							 " endDate = " + eventDTO.getEndDate() +
+							 " templateType = " + eventDTO.getTemplateType() + " exists");
+				throw new EntityExistsException(Organization.class);
+			}
+		}
+		else {
+			logger.debug("createEvent - findByOrganizationNameAsOfDate: orgName = " + eventDTO.getOrganizationName() +
+						 " endDate = " + eventDTO.getEndDate() + " does not exist");
+			throw new NoSuchEntityException(Organization.class);
+		}
+	}
+}
---
+++ event-service/src/main/java/com/rossotti/tournament/dto/EventDTO.java
@@ -0,0 +1,81 @@
+package com.rossotti.tournament.dto;
+
+import com.rossotti.tournament.jpa.model.AvailableLocation;
+import com.rossotti.tournament.jpa.model.AvailableTeam;
+import java.time.LocalDate;
+import java.util.List;
+
+public class EventDTO {
+	private String eventName;
+	private String organizationName;
+	private String templateType;
+	private LocalDate startDate;
+	private LocalDate endDate;
+	private int eventLocationCount;
+	private String sport;
+	private List<AvailableTeam> availableTeams;
+	private List<AvailableLocation> availableLocations;
+
+	public String getEventName() {
+		return eventName;
+	}
+	public void setEventName(String eventName) {
+		this.eventName = eventName;
+	}
+
+	public String getOrganizationName() {
+		return organizationName;
+	}
+	public void setOrganizationName(String organizationName) {
+		this.organizationName = organizationName;
+	}
+
+	public String getTemplateType() {
+		return templateType;
+	}
+	public void setsTemplateType(String templateType) {
+		this.templateType = templateType;
+	}
+
+	public LocalDate getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(LocalDate startDate) {
+		this.startDate = startDate;
+	}
+
+	public LocalDate getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(LocalDate endDate) {
+		this.endDate = endDate;
+	}
+
+	public int getEventLocationCount() {
+		return eventLocationCount;
+	}
+	public void setEventLocationCount(int eventLocationCount) {
+		this.eventLocationCount = eventLocationCount;
+	}
+
+	public String getSport() {
+		return sport;
+	}
+	public void setSport(String sport) {
+		this.sport = sport;
+	}
+
+	public List<AvailableTeam> getTeams() {
+		return availableTeams;
+	}
+	public void setTeams(List<AvailableTeam> availableTeams) {
+		this.availableTeams = availableTeams;
+	}
+
+	public List<AvailableLocation> getAvailableLocations() {
+		return availableLocations;
+	}
+	public void setAvailableLocations(List<AvailableLocation> availableLocations) {
+		this.availableLocations = availableLocations;
+	}
+}
