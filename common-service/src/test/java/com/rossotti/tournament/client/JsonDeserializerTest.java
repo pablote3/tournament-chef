d6b1cc9d312dbb39b0b3e446a58beb9f110ce658
Paul Rossotti <rossotti.paul@gmail.com>
2020-03-12 05:58:09 -0700

    move TemplateFinderService to common-service

--- common-service/src/test/java/com/rossotti/tournament/client/JsonDeserializerTest.java
+++ common-service/src/test/java/com/rossotti/tournament/client/JsonDeserializerTest.java
@@ -1,15 +1,16 @@
 package com.rossotti.tournament.client;
 
 import com.rossotti.tournament.dto.TemplateDTO;
+import com.rossotti.tournament.enumeration.GroupPlay;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@ActiveProfiles(profiles = "development")
 @RunWith(SpringRunner.class)
+@SpringBootTest(classes = com.rossotti.tournament.config.ServiceConfig.class)
 public class JsonDeserializerTest {
 
 	private TemplateFinderService templateFinderService;
@@ -23,7 +24,7 @@ public class JsonDeserializerTest {
 	public void testDeserialize() {
 		try {
 			TemplateDTO template = templateFinderService.locateTemplate("four_x_four_rr");
-			Assert.assertEquals("RoundRobin", template.getGroupPlay1());
+			Assert.assertEquals(GroupPlay.RoundRobin, template.getGroupPlay1());
 		}
 		catch(Exception e) {
 
