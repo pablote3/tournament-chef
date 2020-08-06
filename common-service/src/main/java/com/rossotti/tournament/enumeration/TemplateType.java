f081c06ca0b52758096ddd665ddb825b774be83a
Paul Rossotti <rossotti.paul@gmail.com>
2020-08-05 21:32:08 -0700

    update enum values

--- common-service/src/main/java/com/rossotti/tournament/enumeration/TemplateType.java
+++ common-service/src/main/java/com/rossotti/tournament/enumeration/TemplateType.java
@@ -5,12 +5,10 @@ public enum TemplateType {
 	two_x_four_pp_15D_1L,
 	two_x_four_rr_20D_1L,
 	two_x_four_rr_10D_2L,
-	four_x_three_rr_20D_1L,
 	four_x_three_rr_20D_2L,
-	four_x_three_rr_10D_2L,
 	four_x_four_pp_20D_2L,
 	four_x_four_pp_15D_2L,
-	four_x_four_rr_40D_1L,
+	four_x_four_rr_25D_1L,
 	four_x_four_rr_15D_2L,
 	eight_x_two_pp_20D_2L
 }
