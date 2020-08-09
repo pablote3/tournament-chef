a883f51d569f0e7a3420f272650c9cc4434dbff1
Paul Rossotti <rossotti.paul@gmail.com>
2020-08-09 16:34:11 -0700

    build 2x4 templates

--- common-service/src/main/java/com/rossotti/tournament/enumeration/TemplateType.java
+++ common-service/src/main/java/com/rossotti/tournament/enumeration/TemplateType.java
@@ -5,7 +5,6 @@ public enum TemplateType {
 	two_x_four_pp_15D_1L,
 	two_x_four_rr_20D_1L,
 	two_x_four_rr_10D_2L,
-	four_x_three_rr_20D_2L,
 	four_x_four_pp_20D_2L,
 	four_x_four_pp_15D_2L,
 	four_x_four_rr_25D_1L,
